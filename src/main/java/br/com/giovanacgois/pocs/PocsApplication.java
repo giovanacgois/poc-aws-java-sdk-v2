9618f10f7a715e52dab7721e3e2323a52b951a2a
giovanacgois <giovanacgois@gmail.com>
2022-11-30 23:03:20 -0300

    First commit

---
+++ src/main/java/br/com/giovanacgois/pocs/PocsApplication.java
@@ -0,0 +1,13 @@
+package br.com.giovanacgois.pocs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PocsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PocsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/giovanacgois/pocs/PocsApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.giovanacgois.pocs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PocsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
